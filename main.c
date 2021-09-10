4e83d755173387600070c786deadb09bba3dbb0c
Luis Alejandro Londono Builes <luis.londono@globant.com>
2021-09-10 14:16:17 -0500

    adding multiple games and difficulties

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 20
 
 char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
@@ -7,57 +8,77 @@ void board();
 
 int main()
 {
-    int player = 1, i, choice;
-
+    int player = 1, i, j, out = 1, numberOfGames, choice;
     char mark;
+    char player1[20];
+    char player2[20];
+
     do
     {
-        board();
-        player = (player % 2) ? 1 : 2;
+        printf("please provide the number of games you want to play? ===> 1, 3 ,5\n");
+        scanf("%d", &numberOfGames);
 
-        printf("Player %d, enter a number:  ", player);
-        scanf("%d", &choice);
-
-        mark = (player == 1) ? 'X' : 'O';
-
-        if (choice == 1 && square[1] == '1')
-            square[1] = mark;
-
-        else if (choice == 2 && square[2] == '2')
-            square[2] = mark;
-
-        else if (choice == 3 && square[3] == '3')
-            square[3] = mark;
-
-        else if (choice == 4 && square[4] == '4')
-            square[4] = mark;
-
-        else if (choice == 5 && square[5] == '5')
-            square[5] = mark;
-
-        else if (choice == 6 && square[6] == '6')
-            square[6] = mark;
-
-        else if (choice == 7 && square[7] == '7')
-            square[7] = mark;
-
-        else if (choice == 8 && square[8] == '8')
-            square[8] = mark;
-
-        else if (choice == 9 && square[9] == '9')
-            square[9] = mark;
-
+        if (numberOfGames != 1 && numberOfGames != 3 && numberOfGames != 5)
+        {
+            printf("invalid number\n");
+        }
         else
         {
-            printf("Invalid move ");
-
-            player--;
-            getchar();
+            out = -1;
         }
-        i = checkwin();
+    } while (out == 1);
 
-        player++;
-    } while (i == -1);
+    for (int k = 0; k < numberOfGames; k++)
+    {
+        do
+        {
+            board();
+            player = (player % 2) ? 1 : 2;
+
+            printf("Player %d, enter a number:  ", player);
+            scanf("%d", &choice);
+
+            mark = (player == 1) ? 'X' : 'O';
+
+            if (choice == 1 && square[1] == '1')
+                square[1] = mark;
+
+            else if (choice == 2 && square[2] == '2')
+                square[2] = mark;
+
+            else if (choice == 3 && square[3] == '3')
+                square[3] = mark;
+
+            else if (choice == 4 && square[4] == '4')
+                square[4] = mark;
+
+            else if (choice == 5 && square[5] == '5')
+                square[5] = mark;
+
+            else if (choice == 6 && square[6] == '6')
+                square[6] = mark;
+
+            else if (choice == 7 && square[7] == '7')
+                square[7] = mark;
+
+            else if (choice == 8 && square[8] == '8')
+                square[8] = mark;
+
+            else if (choice == 9 && square[9] == '9')
+                square[9] = mark;
+
+            else
+            {
+                printf("Invalid move ");
+
+                player--;
+                getchar();
+            }
+            i = checkwin();
+
+            player++;
+        } while (i == -1);
+    }
 
     board();
 
@@ -107,7 +128,7 @@ int checkwin()
 
 void board()
 {
-    system("clear");
+    // system("clear");
     printf("\n\n\tTic Tac Toe\n\n");
 
     printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
